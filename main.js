c9f21c666632511415f13b97ee9731409b978e30
Rigoberto Torres <rigo.rock01.ra@gmail.com>
2019-06-13 21:40:13 -0400

    code charge

---
+++ main.js
@@ -0,0 +1,23 @@
+let users = [{
+    "id": 1
+    , "name": "user1"
+    , "id_profile": 1
+}, {
+    "id": 2
+    , "name": "user2"
+    , "id_profile": 2
+}]
+let profiles = [{
+    "id": 1,
+    "name": "administrador"
+}, {
+    "id": 2,
+    "name": "operator"
+}]
+var salida = []
+users.map(user=>{
+    profiles.map(profile=>{
+        profile.id == user.id_profile ? salida.push({"user":user.name, "profile":profile.name}) : 0
+    })
+})
+console.log(salida);
